cf395088e5c8d7a9b33c4c362244a2255afd8bbc
Simon Gerber <simon.gerber@vshn.ch>
2019-10-21 17:25:24 +0200

    Simplify git.checkout_version
    This commit changes `git.checkout_version` to always checkout the
    specified reference as a detached head. This simplifies the code a lot
    and works for any Git commit-ish.
    
    See also: git(1) "<commit-ish>"

--- commodore/git.py
+++ commodore/git.py
@@ -1,6 +1,9 @@
 from git import Repo
+from git.exc import GitCommandError
 
-from .config import VersionType
+class RefError(ValueError):
+    def __init__(self, message):
+        self.message = message
 
 def _normalize_git_ssh(url):
     from url_normalize.url_normalize import normalize_userinfo, normalize_host, \
@@ -24,49 +27,16 @@ def _normalize_git_ssh(url):
     return reconstruct_url(urlparts)
 
 
-class RefError(ValueError):
-    def __init__(self, message):
-        self.message = message
-
-def _tag_name(tagref):
+def checkout_version(repo, ref):
     """
-    Remove `refs/tags` from tag path
+    Checkout `ref` in `repo`. Always checkout as detached HEAD as that
+    massively simplifies the implementation.
     """
-    _refs, _tags, tag = tagref.path.split('/', 2)
-    assert(_refs == "refs")
-    assert(_tags == "tags")
-    return tag
-
-def checkout_version(repo, version):
-    """
-    Checkout `ref` in `repo`.
-    First check if `ref` is a tag on the repository, if so, check out that
-    tag. Otherwise, check if `ref` is a branch, if so, checkout that branch.
-    Otherwise, if `ref` is a commit id, checkout that commit.
-
-    If `ref` is not pointing to a known commit in the repo, raise an
-    Exception.
-    """
-    if version.type == VersionType.tag:
-        tags = list(map(_tag_name, repo.tags))
-        try:
-            tidx = tags.index(version.ref)
-        except ValueError as e:
-            raise RefError(f"Unknown tag {version.ref}") from e
-        repo.head.reference = tags[tidx]
-    elif version.type == VersionType.branch:
-        branches = list(map(lambda h: h.name, repo.heads))
-        try:
-            bidx = branches.index(version.ref)
-        except ValueError as e:
-            raise RefError(f"Unknown branch {version.ref}") from e
-        repo.head.reference = repo.heads[bidx]
-    elif version.type == VersionType.ref:
-        repo.head.reference = repo.commit(version.ref)
-    else:
-        raise RefError(f"Unknown version type {version.type.name}")
-
-    repo.head.reset(index=True, working_tree=True)
+    try:
+        repo.head.reference = repo.commit(ref)
+        repo.head.reset(index=True, working_tree=True)
+    except GitCommandError as e:
+        raise RefError(f"Failed to checkout revision {ref}") from e
 
 def clone_repository(repository_url, directory):
     return Repo.clone_from(_normalize_git_ssh(repository_url), directory)
