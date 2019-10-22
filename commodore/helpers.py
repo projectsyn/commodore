826a7a295ddabec1cbc7cd109877ec324aac6f7b
Simon Gerber <simon.gerber@vshn.ch>
2019-10-22 14:18:19 +0200

    Explicitly handle exceptions for top-level method calls
    Raise ClickException() to exit commodore with a failure exitcode.

--- commodore/helpers.py
+++ commodore/helpers.py
@@ -1,11 +1,19 @@
 import json, requests
+from requests.exceptions import ConnectionError
 from url_normalize import url_normalize
+
+class ApiError(Exception):
+    def __init__(self, message):
+        self.message = message
 
 def api_request(api_url, type, customer, cluster):
     if type != "inventory" and type != "targets":
         print(f"Unknown API endpoint {type}")
         return {}
-    r = requests.get(url_normalize(f"{api_url}/{type}/{customer}/{cluster}"))
+    try:
+        r = requests.get(url_normalize(f"{api_url}/{type}/{customer}/{cluster}"))
+    except ConnectionError as e:
+        raise ApiError(f"Unable to connect to SYNventory at {api_url}") from e
     resp = json.loads(r.text)
     if r.status_code == 404:
         print(resp['message'])
