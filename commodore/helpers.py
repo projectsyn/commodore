0f1049b833ae4620e3e1120eb5d66f79653e43ee
Simon Gerber <simon.gerber@vshn.ch>
2019-10-28 17:14:00 +0100

    Implement helpers for loading and dumping multi-document YAML

--- commodore/helpers.py
+++ commodore/helpers.py
@@ -5,16 +5,40 @@ from ruamel.yaml import YAML
 from pathlib import Path as P
 
 def yaml_load(file):
+    """
+    Load single-document YAML and return document
+    """
     yaml=YAML(typ='safe')
     with open(file, 'r') as f:
         return yaml.load(f)
 
+def yaml_load_all(file):
+    """
+    Load multi-document YAML and return documents in list
+    """
+    yaml=YAML(typ='safe')
+    with open(file, 'r') as f:
+        return list(yaml.load_all(f))
+
 def yaml_dump(obj, file):
+    """
+    Dump obj as single-document YAML
+    """
     yaml=YAML()
     yaml.default_flow_style = False
     yaml.indent(mapping=2, sequence=4, offset=2)
     with open(file, 'w') as outf:
         yaml.dump(obj, outf)
+
+def yaml_dump_all(obj, file):
+    """
+    Dump obj as multi-document YAML
+    """
+    yaml=YAML()
+    yaml.default_flow_style = False
+    yaml.indent(mapping=2, sequence=4, offset=2)
+    with open(file, 'w') as outf:
+        yaml.dump_all(obj, outf)
 
 class ApiError(Exception):
     def __init__(self, message):
