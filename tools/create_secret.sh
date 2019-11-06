aa3d19bba30ba9ef23c84b0527ade16211d56f1c
Simon Gerber <simon.gerber@vshn.ch>
2019-11-06 16:21:04 +0100

    Fix create_secret helper to correctly setup secret references

--- tools/create_secret.sh
+++ tools/create_secret.sh
@@ -5,12 +5,14 @@ set -euo pipefail
 readonly command='kapitan refs'
 
 usage() {
-    echo "$0 -p <vault_path> [-r <reference_name>]"
-    echo "   -p / --vault-path path/to/secret/in/vault"
-    echo "   -r / --commodore-ref refname [default: vault-path]"
+    echo "$0 -p <vault_path> -k <key in vault secret> [-r <reference_name>]"
+    echo "   -p path/to/secret/in/vault"
+    echo "   -k key in vault secret"
+    echo "   -r refname [default: vault_path/key]"
 }
 
 vault_path=
+key=
 refname=
 
 echo "$#"
@@ -19,7 +21,7 @@ if [[ "$#" -eq 0 ]]; then
     exit 1
 fi
 
-while getopts 'hp:r:' opt; do
+while getopts 'hp:r:k:' opt; do
     case "$opt" in
         h)
             usage
@@ -27,6 +29,7 @@ while getopts 'hp:r:' opt; do
             ;;
         p) vault_path="$OPTARG" ;;
         r) refname="$OPTARG" ;;
+        k) key="$OPTARG" ;;
         *)
             usage >&2
             exit 1
@@ -35,9 +38,9 @@ while getopts 'hp:r:' opt; do
 done
 
 if [[ -z "$refname" ]]; then
-    refname="$vault_path"
+    refname="$vault_path/$key"
 fi
 
-echo -n "$vault_path" | $command --refs-path ./catalog/refs -t cluster -w "vaultkv:${refname}" -f-
+echo -n "$vault_path:$key" | $command --refs-path ./catalog/refs -t cluster -w "vaultkv:${refname}" -f-
 
 # vim: set et sw=4 ts=4 :
