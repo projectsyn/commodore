3a1b23f08716d3c0dc4de78127ecf2fb92b691df
Simon Gerber <simon.gerber@vshn.ch>
2019-12-16 16:05:33 +0100

    Remove hard-coded references to python3.6 in helm binding build process
    This commit changes the helper script and Dockerfile for building the
    Kapitan helm binding to support arbitrary python versions.

--- tools/build_kapitan_helm_binding.sh
+++ tools/build_kapitan_helm_binding.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
-readonly helm_binding_path="${VIRTUAL_ENV}/lib/python3.6/site-packages/kapitan/inputs/helm"
+# TODO: nicer pipeline
+readonly pyver="python$(python -V 2>&1 | cut -d' ' -f2 | cut -d. -f1-2)"
+readonly helm_binding_path="${VIRTUAL_ENV}/lib/${pyver}/site-packages/kapitan/inputs/helm"
 readonly dockerfile="$(dirname "$0")/Dockerfile.build_kapitan_helm_binding"
 
-docker build -t kapitan-helm-build -f "${dockerfile}" "$VIRTUAL_ENV"
+docker build --build-arg PY_VER="$pyver" -t kapitan-helm-build -f "${dockerfile}" "$VIRTUAL_ENV"
 docker create -ti --name khb kapitan-helm-build:latest bash
 docker cp khb:/virtualenv/kapitan/inputs/helm/libtemplate.so "$helm_binding_path"
 docker rm -f khb
