dfd4c01fdf9cc763d90bcf0fe8b7a169e83475f9
Simon Rüegg <simon@rueggs.ch>
2020-05-12 18:36:46 +0200

    Implement review findings
    Signed-off-by: Simon Rüegg <simon@rueggs.ch>

--- tests/test_git.py
+++ tests/test_git.py
@@ -2,10 +2,11 @@
 Unit-tests for git
 """
 
-import commodore.git as git
+from commodore import git
+from pathlib import Path
 
 
-def test_reconstruct_api_response(tmp_path):
+def test_reconstruct_api_response(tmp_path: Path):
     repo_path = tmp_path / 'test-repo'
     repo_path.mkdir()
     repo = git.create_repository(repo_path)
