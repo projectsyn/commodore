d4cc5082c8c5ca49e8669f39542b6a5e04aab6bc
Simon Gerber <simon.gerber@vshn.ch>
2020-06-15 15:35:45 +0200

    Update Authors, LICENSE and module description

--- commodore/__init__.py
+++ commodore/__init__.py
@@ -2,9 +2,6 @@
 Commodore. Build dynamic inventories and compile catalogs with Kapitan
 """
 
-__author__ = 'Simon Gerber'
-__email__ = 'simon.gerber@vshn.ch'
-__license__ = 'BSD'
 __url__ = 'https://git.vshn.net/syn/commodore/'
 __git_version__ = "0"
 
