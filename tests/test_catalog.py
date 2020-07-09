9e7c44ea0ec6e845046e7ff32ef32d5affe8b6b6
Simon Rüegg <simon@rueggs.ch>
2020-07-09 09:13:19 +0200

    Write test for commit message

---
+++ tests/test_catalog.py
@@ -0,0 +1,14 @@
+"""
+Tests for catalog internals
+"""
+
+from commodore.catalog import _render_catalog_commit_msg
+from commodore.config import Config
+
+
+def test_catalog_commit_message():
+    config = Config("https://syn.example.com", "token", "ssh://git@git.example.com", False)
+
+    commit_message = _render_catalog_commit_msg(config)
+    assert not commit_message.startswith('\n')
+    assert commit_message.startswith('Automated catalog update from Commodore\n\n')
