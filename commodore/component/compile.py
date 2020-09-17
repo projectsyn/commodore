4624fbdfe4e9d8843f2eaf6760db4db1c9822604
Chris Glass <tribaal@gmail.com>
2020-09-17 10:25:12 +0200

    Update commodore/component/compile.py
    Co-authored-by: Simon Rüegg <simon@rueggs.ch>

--- commodore/component/compile.py
+++ commodore/component/compile.py
@@ -75,7 +75,8 @@ classes:
 
         # Fake Argo CD lib
         # We plug "Fake" argoCD configuration here because we don't actually
-        # want to recompile that stuff each when compiling a single component.
+        # We plug "fake" Argo CD library here because every component relies on it
+        # and we don't want to provide it every time when compiling a single component.
         (temp_dir / 'dependencies/lib').mkdir(exist_ok=True)
         with open(temp_dir / 'dependencies/lib/argocd.libjsonnet', 'w') as file:
             file.write("""
