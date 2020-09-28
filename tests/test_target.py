d802c4239b644887f06e7189bcb843956a398dac
Christian Haeusler <794584+corvus-ch@users.noreply.github.com>
2020-09-28 14:11:03 +0200

    Update cluster tests

--- tests/test_target.py
+++ tests/test_target.py
@@ -6,6 +6,7 @@ import os
 import click
 import pytest
 
+from pathlib import Path as P
 from textwrap import dedent
 
 from commodore import cluster
@@ -17,81 +18,68 @@ def data():
     Setup test data
     """
 
-    data = {
-        'cluster': {
-            'id': 'mycluster',
-            'tenant': 'mytenant',
-            'facts': {
-                'distribution': 'rancher',
-                'cloud': 'cloudscale',
-            },
+    return {
+        'id': 'mycluster',
+        'tenant': 'mytenant',
+        'facts': {
+            'distribution': 'rancher',
+            'cloud': 'cloudscale',
         },
-        'components': [],
-        'catalog': 'ssh://git@git.example.com/cluster-catalogs/mycluster',
+        'gitRepo': {
+            'url': 'ssh://git@git.example.com/cluster-catalogs/mycluster',
+        },
     }
-    return data
 
 
-def test_render_target(data):
-    target = cluster._full_target(data['cluster'], data['components'], data['catalog'])
-    facts = data['cluster']['facts']
-    all_classes = [f"defaults.{cn}" for cn in data['components']] + [
-        'global.common',
-        f"global.distribution.{facts['distribution']}",
-        f"global.cloud.{facts['cloud']}",
-        f"{data['cluster']['tenant']}.{data['cluster']['id']}"]
+def test_render_target(tmp_path):
+    os.chdir(tmp_path)
+    bar_defaults = P('inventory/classes/defaults/bar.yml')
+    os.makedirs(bar_defaults.parent, exist_ok=True)
+    bar_defaults.touch()
+    target = cluster.render_target('foo', ['bar', 'baz'])
+    classes = [
+        'params.foo',
+        'defaults.bar',
+        'global.commodore',
+    ]
     assert target != ""
-    assert len(target['classes']) == len(all_classes), \
+    print(target)
+    assert len(target['classes']) == len(classes), \
         "rendered target includes different amount of classes"
-    for i in range(len(all_classes)):
-        assert target['classes'][i] == all_classes[i]
-    assert target['parameters']['target_name'] == 'cluster'
+    for i in range(len(classes)):
+        assert target['classes'][i] == classes[i]
 
 
-def test_optional_fact_region(data):
-    data['cluster']['facts']['region'] = 'rma1'
-    target = cluster._full_target(data['cluster'], data['components'], data['catalog'])
-    facts = data['cluster']['facts']
-    assert f"global.cloud.{facts['cloud']}.{facts['region']}" in target['classes']
-
-
-def test_optional_fact_region_empty(data):
-    data['cluster']['facts']['region'] = ''
-    target = cluster._full_target(data['cluster'], data['components'], data['catalog'])
-    facts = data['cluster']['facts']
-    assert f"global.cloud.{facts['cloud']}.{facts['region']}" not in target['classes']
-
-
-def test_optional_fact_lieutenant_instance(data):
-    data['cluster']['facts']['lieutenant-instance'] = 'lieutenant-dev'
-    target = cluster._full_target(data['cluster'], data['components'], data['catalog'])
-    facts = data['cluster']['facts']
-    assert f"global.lieutenant-instance.{facts['lieutenant-instance']}" in target['classes']
+def test_render_params(data):
+    params = cluster.render_params(data, 'foo')
+    assert params['parameters']['target_name'] == 'foo'
+    assert params['parameters']['cluster']['name'] == 'mycluster'
+    assert params['parameters']['cluster']['catalog_url'] == 'ssh://git@git.example.com/cluster-catalogs/mycluster'
+    assert params['parameters']['cluster']['tenant'] == 'mytenant'
+    assert params['parameters']['cluster']['dist'] == 'rancher'
+    assert params['parameters']['facts'] == data['facts']
+    assert params['parameters']['cloud']['provider'] == 'cloudscale'
+    assert params['parameters']['customer']['name'] == 'mytenant'
 
 
 def test_missing_facts(data):
-    data['cluster']['facts'].pop('cloud')
+    data['facts'].pop('cloud')
     with pytest.raises(click.ClickException):
-        cluster._full_target(data['cluster'], data['components'], data['catalog'])
+        cluster.render_params(data, 'foo')
 
 
 def test_empty_facts(data):
-    data['cluster']['facts']['cloud'] = ''
+    data['facts']['cloud'] = ''
     with pytest.raises(click.ClickException):
-        cluster._full_target(data['cluster'], data['components'], data['catalog'])
+        cluster.render_params(data, 'foo')
 
 
 def test_reconstruct_api_response(tmp_path):
     os.chdir(tmp_path)
-    targetyml = tmp_path / 'cluster.yml'
-    with open(targetyml, 'w') as file:
-        file.write(dedent('''
-            classes:
-            - defaults.argocd
-            - global.common
-            - global.distribution.k3d
-            - global.cloud.localdev
-            - t-delicate-pine-3938.c-twilight-water-9032
+    file = cluster.params_file('foo')
+    os.makedirs(file.parent, exist_ok=True)
+    with open(file, 'w') as f:
+        f.write(dedent('''
             parameters:
               cluster:
                 catalog_url: ssh://git@git.vshn.net/syn-dev/cluster-catalogs/srueg-k3d-int.git
@@ -103,7 +91,7 @@ def test_reconstruct_api_response(tmp_path):
                 region: north
               target_name: cluster'''))
 
-    api_response = cluster.reconstruct_api_response(targetyml)
+    api_response = cluster.reconstruct_api_response('foo')
     assert api_response['id'] == 'c-twilight-water-9032'
     assert api_response['tenant'] == 't-delicate-pine-3938'
     assert api_response['facts']['distribution'] == 'k3d'
@@ -112,11 +100,12 @@ def test_reconstruct_api_response(tmp_path):
 
 def test_reconstruct_api_response_missing_fact(tmp_path):
     os.chdir(tmp_path)
-    targetyml = tmp_path / 'cluster.yml'
-    with open(targetyml, 'w') as file:
-        file.write(dedent('''
+    file = cluster.params_file('foo')
+    os.makedirs(file.parent, exist_ok=True)
+    with open(file, 'w') as f:
+        f.write(dedent('''
             classes: []
             parameters: {}'''))
 
     with pytest.raises(KeyError):
-        cluster.reconstruct_api_response(targetyml)
+        cluster.reconstruct_api_response('foo')
