2dc1f384219c769973b1730a628dfb005d30dc71
Simon Gerber <simon.gerber@vshn.ch>
2020-10-19 10:49:04 +0200

    Silence pylint error for class inheriting from NamedTuple
    Python 3.9 changed typing.NamedTuple to be a function that can be
    inherited from. This trips up pylint, cf.
    https://github.com/PyCQA/pylint/issues/3876.

--- commodore/config.py
+++ commodore/config.py
@@ -4,6 +4,10 @@ from pathlib import Path as P
 from git import Repo
 
 
+# Python 3.9 changed typing.NamedTuple to be a function that can be inherited
+# from. This trips up pylint, cf. https://github.com/PyCQA/pylint/issues/3876.
+# pylint: disable=inherit-non-class
+# pylint: disable=too-few-public-methods
 class Component(NamedTuple):
     name: str
     repo: Repo
