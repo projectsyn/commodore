7adce70b50cbfede7f8c1da5326400649d2828d4
Christian Haeusler <794584+corvus-ch@users.noreply.github.com>
2020-10-22 14:56:27 +0200

    Test Cluster.config_git_repo_revision

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -97,3 +97,30 @@ def test_config_repo_url(data):
     }
     cluster = Cluster(data["config"], data["cluster"], data["tenant"])
     assert "ssh://git@example.com/tenant.git" == cluster.config_repo_url
+
+
+def test_config_git_repo_revision(data):
+    cluster = Cluster(data["config"], data["cluster"], data["tenant"])
+    assert not cluster.config_git_repo_revision
+
+    set_on_tenant = data.copy()
+    set_on_tenant["tenant"]["gitRepoRevision"] = "v1.2.3"
+    cluster = Cluster(
+        set_on_tenant["config"], set_on_tenant["cluster"], set_on_tenant["tenant"]
+    )
+    assert "v1.2.3" == cluster.config_git_repo_revision
+
+    set_on_cluster = data.copy()
+    set_on_cluster["cluster"]["gitRepoRevision"] = "v3.2.1"
+    cluster = Cluster(
+        set_on_cluster["config"], set_on_cluster["cluster"], set_on_cluster["tenant"]
+    )
+    assert "v3.2.1" == cluster.config_git_repo_revision
+
+    set_on_both = data.copy()
+    set_on_both["cluster"]["gitRepoRevision"] = "v2.3.1"
+    set_on_both["tenant"]["gitRepoRevision"] = "v1.2.3"
+    cluster = Cluster(
+        set_on_both["config"], set_on_both["cluster"], set_on_both["tenant"]
+    )
+    assert "v2.3.1" == cluster.config_git_repo_revision
