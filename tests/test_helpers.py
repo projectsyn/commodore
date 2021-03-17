639ecc1738b6bc2081a4ab458045b625e8a3eefd
Simon Gerber <simon.gerber@vshn.ch>
2021-03-17 11:30:12 +0100

    Use block-scalars in Commodore's YAML dump helpers for multi-line strings
    By providing a custom implementation of `represent_str` and injecting it
    into PyYAML's dumper with `yaml.add_representer()`, we can control how
    multi-line strings are rendered.
    
    This commit enables block-scalar style without trailing newline for any
    input strings which contain `\n`.
    
    This will improve readability of catalog diffs for manifests which
    contain multi-line strings and have been processed by Commodore's
    postprocessing step.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,6 +2,9 @@
 Unit-tests for helpers
 """
 from pathlib import Path
+from typing import Callable
+import textwrap
+import pytest
 
 import commodore.helpers as helpers
 from commodore.config import Config
@@ -27,3 +30,107 @@ def test_clean_working_tree(tmp_path: Path):
     assert d.is_dir()
     helpers.clean_working_tree(cfg)
     assert d.is_dir()
+
+
+def _test_yaml_dump_fun(
+    dumpfunc: Callable[[str, Path], None], tmp_path: Path, input, expected
+):
+    output = tmp_path / "test.yaml"
+    dumpfunc(input, output)
+    with open(output) as f:
+        data = "".join(f.readlines())
+    assert expected == data
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (
+            {"a": 1, "b": "test"},
+            textwrap.dedent(
+                """
+                a: 1
+                b: test
+                """
+            )[1:],
+        ),
+        (
+            {"a": [1, 2], "b": "test"},
+            textwrap.dedent(
+                """
+                a:
+                - 1
+                - 2
+                b: test
+                """
+            )[1:],
+        ),
+        (
+            {"a": {"test": 1}, "b": "test"},
+            textwrap.dedent(
+                """
+                a:
+                  test: 1
+                b: test
+                """
+            )[1:],
+        ),
+        (
+            {"a": "first line\nsecond line", "b": "test"},
+            textwrap.dedent(
+                """
+                a: |-
+                  first line
+                  second line
+                b: test
+                """
+            )[1:],
+        ),
+    ],
+)
+def test_yaml_dump(tmp_path: Path, input, expected):
+    _test_yaml_dump_fun(helpers.yaml_dump, tmp_path, input, expected)
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (
+            [{"a": 1}, {"b": "test"}],
+            textwrap.dedent(
+                """
+                    a: 1
+                    ---
+                    b: test
+                    """
+            )[1:],
+        ),
+        (
+            [{"a": {"test": "first line\nsecond line"}}, {"b": "test"}],
+            textwrap.dedent(
+                """
+                a:
+                  test: |-
+                    first line
+                    second line
+                ---
+                b: test
+                """
+            )[1:],
+        ),
+        (
+            [{"a": [1, 2]}, {"b": "test"}],
+            textwrap.dedent(
+                """
+                a:
+                - 1
+                - 2
+                ---
+                b: test
+                """
+            )[1:],
+        ),
+    ],
+)
+def test_yaml_dump_all(tmp_path: Path, input, expected):
+    _test_yaml_dump_fun(helpers.yaml_dump_all, tmp_path, input, expected)
