b3695efdf57e8e9782a40e8c05a82c648831f02c
haasad <haasad@users.noreply.github.com>
2021-06-25 11:47:54 +0200

    Lint with black

--- commodore/copy_helm_bindings.py
+++ commodore/copy_helm_bindings.py
@@ -12,16 +12,23 @@ def main():
     input_source_dir = os.path.join(commodore_dir, "helm_bindings", "inputs")
     input_target_dir = os.path.join(kapitan_dir, "inputs", "helm")
 
-    dependency_manager_source_dir = os.path.join(commodore_dir, "helm_bindings", "dependency_manager")
-    dependency_manager_target_dir = os.path.join(kapitan_dir, "dependency_manager", "helm")
+    dependency_manager_source_dir = os.path.join(
+        commodore_dir, "helm_bindings", "dependency_manager"
+    )
+    dependency_manager_target_dir = os.path.join(
+        kapitan_dir, "dependency_manager", "helm"
+    )
 
     for f in ["helm_binding.py", "libtemplate.so"]:
-        shutil.copyfile(os.path.join(input_source_dir, f),
-                        os.path.join(input_target_dir, f))
+        shutil.copyfile(
+            os.path.join(input_source_dir, f), os.path.join(input_target_dir, f)
+        )
 
     for f in ["helm_fetch_binding.py", "helm_fetch.so"]:
-        shutil.copyfile(os.path.join(dependency_manager_source_dir, f),
-                        os.path.join(dependency_manager_target_dir, f))
+        shutil.copyfile(
+            os.path.join(dependency_manager_source_dir, f),
+            os.path.join(dependency_manager_target_dir, f),
+        )
 
 
 if __name__ == "__main__":
