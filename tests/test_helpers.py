49c65d777831a7fc11f4a481c0427473b50f471b
Simon Gerber <simon.gerber@vshn.ch>
2021-12-31 14:47:55 +0100

    Add tests for `lieutenant_query`

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from typing import Callable
 import textwrap
 import pytest
+import responses
+from url_normalize import url_normalize
 
 import commodore.helpers as helpers
 from commodore.config import Config
@@ -164,3 +166,89 @@ def test_yaml_dump_all(tmp_path: Path, input, expected):
 def test_sliding_window(sequence, winsize, expected):
     windows = list(helpers.sliding_window(sequence, winsize))
     assert windows == expected
+
+
+def _verify_call_status(query_url):
+    assert len(responses.calls) == 1
+    call = responses.calls[0]
+    assert "Authorization" in call.request.headers
+    assert call.request.headers["Authorization"] == "Bearer token"
+    assert call.request.url == query_url
+
+
+@responses.activate
+def test_lieutenant_query_connection_error():
+    api_url = "https://syn.example.com"
+    with pytest.raises(
+        helpers.ApiError, match=f"Unable to connect to Lieutenant at {api_url}"
+    ):
+        helpers.lieutenant_query(api_url, "token", "clusters", "c-cluster-id-1234")
+
+    _verify_call_status(f"{api_url}/clusters/c-cluster-id-1234")
+
+
+@pytest.mark.parametrize(
+    "response,expected",
+    [
+        (
+            {
+                "status": 403,
+                "json": {"reason": "Unauthorized"},
+            },
+            "API returned 403: Unauthorized",
+        ),
+        (
+            {
+                "status": 403,
+                "json": {},
+            },
+            "API returned 403: ",
+        ),
+        (
+            {
+                "status": 500,
+                "json": {"reason": "Unexpected frobulation"},
+            },
+            "API returned 500: Unexpected frobulation",
+        ),
+        (
+            {
+                "status": 200,
+                "body": "No JSON in response",
+            },
+            "Client error: Unable to parse JSON",
+        ),
+        (
+            {
+                "status": 403,
+                "body": "No JSON in response",
+            },
+            "Client error: Unable to parse JSON",
+        ),
+    ],
+)
+@responses.activate
+def test_lieutenant_query_response_errors(response, expected):
+    base_url = "https://syn.example.com/"
+
+    query_url = url_normalize(f"{base_url}/clusters/")
+
+    if "json" in response:
+        responses.add(
+            responses.GET,
+            query_url,
+            status=response["status"],
+            json=response["json"],
+        )
+    elif "body" in response:
+        responses.add(
+            responses.GET,
+            query_url,
+            status=response["status"],
+            body=response["body"],
+        )
+
+    with pytest.raises(helpers.ApiError, match=expected):
+        helpers.lieutenant_query(base_url, "token", "clusters", "")
+
+    _verify_call_status(query_url)
