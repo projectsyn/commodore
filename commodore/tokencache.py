cbee2976756303410d888e702371f77583ee829c
Fabian Fischer <glrf@users.noreply.github.com>
2022-03-22 17:12:12 +0100

    Add tests for tokencache

--- commodore/tokencache.py
+++ commodore/tokencache.py
@@ -3,12 +3,12 @@ from typing import Optional
 from pathlib import Path
 import json
 
-cache_name = "{home}/.cache/commodore/token".format(home=Path.home())
+cache_name = f"{Path.home()}/.cache/commodore/token"
 
 
 def save(lieutenant: str, token: str):
     try:
-        with open(cache_name, "r") as f:
+        with open(cache_name, "r", encoding="utf-8") as f:
             cache = json.load(f)
     except (IOError, FileNotFoundError):
         cache = {}
@@ -16,14 +16,14 @@ def save(lieutenant: str, token: str):
     cache[lieutenant] = token
 
     os.makedirs(os.path.dirname(cache_name), exist_ok=True)
-    with open(cache_name, "w") as f:
+    with open(cache_name, "w", encoding="utf-8") as f:
         f.write(json.dumps(cache, indent=1))
 
 
 def get(lieutenant: str) -> Optional[str]:
     try:
-        with open(cache_name, "r") as f:
+        with open(cache_name, "r", encoding="utf-8") as f:
             cache = json.load(f)
     except (IOError, FileNotFoundError):
-        return
+        return None
     return cache[lieutenant]
