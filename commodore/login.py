cffa3af551ba80b89fde78ed07448c0b56a881e8
Fabian Fischer <glrf@users.noreply.github.com>
2022-03-22 11:05:28 +0100

    Remove unecessary dependency on fastapi

--- commodore/login.py
+++ commodore/login.py
@@ -3,43 +3,49 @@ import threading
 import webbrowser
 import json
 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
 from oauthlib.oauth2 import WebApplicationClient
 
 import requests
 
-import uvicorn
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
 from .config import Config
 
-client = WebApplicationClient("syn-lieutenant-dev")
 
-app = FastAPI()
+class OIDCServer(BaseHTTPRequestHandler):
+    client = WebApplicationClient("syn-lieutenant-dev")
+
+    def do_GET(self):
+        print(self.path)
+        query_components = parse_qs(urlparse(self.path).query)
+        code = query_components["code"]
+        token_url, headers, body = self.client.prepare_token_request(
+            "https://id.test.vshn.net/auth/realms/VSHN-main-dev-realm/protocol/openid-connect/token",
+            redirect_url="http://localhost:8000",
+            code=code[0],
+        )
+        token_response = requests.post(
+            token_url,
+            headers=headers,
+            data=body,
+        )
+
+        print(
+            self.client.parse_request_body_response(json.dumps(token_response.json()))[
+                "id_token"
+            ]
+        )
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(str.encode(success_page))
+
+        return
 
 
-@app.get("/")
-async def callback(code: str = ""):
-    token_url, headers, body = client.prepare_token_request(
-        "https://id.test.vshn.net/auth/realms/VSHN-main-dev-realm/protocol/openid-connect/token",
-        # authorization_response="http://localhost:8000",
-        redirect_url="http://localhost:8000",
-        code=code,
-    )
-    token_response = requests.post(
-        token_url,
-        headers=headers,
-        data=body,
-    )
-
-    print(
-        client.parse_request_body_response(json.dumps(token_response.json()))[
-            "id_token"
-        ]
-    )
-    return HTMLResponse(
-        status_code=200,
-        content="""
+success_page = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -70,20 +76,21 @@ async def callback(code: str = ""):
 	</div>
 </body>
 </html>
-""",
-    )
+"""
 
 
-def _start_server():
-    uvicorn.run(app, port=8000, log_level="error")
+def start_server():
+    webServer = HTTPServer(("localhost", 8000), OIDCServer)
+    webServer.serve_forever()
 
 
 def login(config: Config):
-    server_thread = threading.Thread(target=_start_server)
+    server_thread = threading.Thread(target=start_server)
     server_thread.start()
 
     # TODO(glrf) That's racy
-    request_uri = client.prepare_request_uri(
+    c = WebApplicationClient("syn-lieutenant-dev")
+    request_uri = c.prepare_request_uri(
         "https://id.test.vshn.net/auth/realms/VSHN-main-dev-realm/protocol/openid-connect/auth",
         redirect_uri="http://localhost:8000",
         scope=["openid", "email", "profile"],
