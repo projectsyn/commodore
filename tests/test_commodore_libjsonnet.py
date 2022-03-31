42957a351429f94e0fdf1b5b8f2abaaaf0494cb1
Simon Gerber <simon.gerber@vshn.ch>
2022-03-31 11:01:32 +0200

    Add tests for `yaml_load()` and `yaml_load_all()`

--- tests/test_commodore_libjsonnet.py
+++ tests/test_commodore_libjsonnet.py
@@ -3,7 +3,7 @@ import json
 import os
 
 from pathlib import Path
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 from commodore.postprocess.jsonnet import _import_cb, _native_callbacks
 
@@ -16,7 +16,7 @@ import yaml
 TESTS_DIR = Path(__file__).parent / "jsonnet"
 
 
-def discover_tc():
+def discover_tc() -> List[str]:
     files = {
         f.stem
         for f in TESTS_DIR.iterdir()
@@ -26,7 +26,7 @@ def discover_tc():
     return list(files)
 
 
-def tc_files(tc: str) -> (str, Path):
+def tc_files(tc: str) -> (Path, Path, Path):
     return (
         TESTS_DIR / f"{tc}.jsonnet",
         TESTS_DIR / f"{tc}.yaml",
@@ -34,11 +34,65 @@ def tc_files(tc: str) -> (str, Path):
     )
 
 
-def render_jsonnet(tmp_path: Path, inputf: Path, invf: Path):
+def write_testdata(tmp_path: Path):
+    testdata1 = [
+        {
+            "metadata": {
+                "name": "obj1",
+            },
+            "spec": {
+                "a": "a",
+                "b": "b",
+                "c": "c",
+                "d": "d",
+            },
+        },
+        {
+            "metadata": {
+                "name": "obj2",
+                "namespace": "foo",
+            },
+            "spec": 5,
+        },
+        {
+            "metadata": {
+                "name": "obj3",
+                "namespace": "test",
+            },
+            "spec": {"a": [1, 2, 3], "b": [4, 5, 6]},
+        },
+    ]
+    testdata2 = [
+        {
+            "metadata": {
+                "name": "obj4",
+                "namespace": "test",
+            },
+            "spec": {
+                "list": [1],
+            },
+        },
+        {
+            "metadata": {"name": "obj5"},
+            "spec": {"value": "aaa"},
+        },
+    ]
+
+    with open(tmp_path / "test0.yaml", "w", encoding="utf-8") as tf:
+        yaml.dump_all([testdata1[0]], tf)
+
+    with open(tmp_path / "test1.yaml", "w", encoding="utf-8") as tf:
+        yaml.dump_all(testdata1, tf)
+
+    with open(tmp_path / "test2.yaml", "w", encoding="utf-8") as tf:
+        yaml.dump_all(testdata2, tf)
+
+
+def render_jsonnet(tmp_path: Path, inputf: Path, invf: Path, **kwargs):
     inv = {}
     if invf.is_file():
-        with open(invf) as invf:
-            inv = yaml.safe_load(invf)
+        with open(invf) as invfh:
+            inv = yaml.safe_load(invfh)
 
     def _inventory() -> Dict[str, Any]:
         return inv
@@ -50,6 +104,7 @@ def render_jsonnet(tmp_path: Path, inputf: Path, invf: Path):
         str(inputf),
         import_callback=functools.partial(_import_cb, tmp_path),
         native_callbacks=_native_cb,
+        ext_vars=kwargs,
     )
     return json.loads(resstr)
 
@@ -58,7 +113,62 @@ def render_jsonnet(tmp_path: Path, inputf: Path, invf: Path):
     "tc",
     discover_tc(),
 )
-def test_jsonnet(tmp_path: Path, tc):
+def test_jsonnet(tmp_path: Path, tc: str):
+    """Test jsonnet functions.
+
+    Functions can expect the following files to be present in the directory indicated by external variable `work_dir`:
+
+    * test0.yaml:
+    ```
+    ---
+    metadata:
+      name: obj1
+    spec:
+      a: a
+      b: b
+      c: c
+      d: d
+    ```
+
+    * test1.yaml:
+    ```
+    ---
+    metadata:
+      name: obj1
+    spec:
+      a: a
+      b: b
+      c: c
+      d: d
+    ---
+    metadata:
+      name: obj2
+      namespace: foo
+    spec: 5
+    ---
+    metadata:
+      name: obj3
+      namespace: test
+    spec:
+      a: [1,2,3]
+      b: [4,5,6]
+    ```
+
+    * test2.yaml
+    ```
+    ---
+    metadata:
+      name: obj4
+      namespace: test
+    spec:
+      list:
+      - 1
+    ---
+    metadata:
+      name: obj5
+    spec:
+      value: aaa
+    """
     inputf, invf, expectedf = tc_files(tc)
     os.makedirs(tmp_path / "lib")
     resp = requests.get(
@@ -66,7 +176,8 @@ def test_jsonnet(tmp_path: Path, tc):
     )
     with open(tmp_path / "lib" / "kube.libjsonnet", "w") as f:
         f.write(resp.text)
-    result = render_jsonnet(tmp_path, inputf, invf)
+    write_testdata(tmp_path)
+    result = render_jsonnet(tmp_path, inputf, invf, work_dir=str(tmp_path))
     with open(expectedf) as e:
         expected = json.load(e)
 
