7435cbc7adf46c1f5721ae949170a009bb204e2c
Simon Gerber <simon.gerber@vshn.ch>
2022-05-19 14:24:50 +0200

    Add tests for package template with additional test cases

--- tests/test_package_template.py
+++ tests/test_package_template.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
 from pathlib import Path
 from subprocess import call
 
 import click
 import pytest
+import yaml
 
 from commodore.config import Config
 from commodore.package.template import PackageTemplater
@@ -14,20 +16,34 @@ def call_package_new(
     golden="--no-golden-tests",
     output_dir: str = "",
     template_version: str = "",
+    additional_test_cases: list[str] = [],
 ):
+    atc_args = " ".join(f"-t {case}" for case in additional_test_cases)
     exit_status = call(
         f"commodore -d {tmp_path} -vvv package new {package_name}"
-        + f" {golden} {output_dir} {template_version}",
+        + f" {golden} {output_dir} {template_version} {atc_args}",
         shell=True,
     )
     assert exit_status == 0
 
 
 @pytest.mark.parametrize("output_dir", ["", "--output-dir={0}"])
-def test_run_package_new_command(tmp_path: Path, output_dir: str):
+@pytest.mark.parametrize(
+    "additional_test_cases",
+    [
+        [],
+        ["foo"],
+        ["foo", "bar"],
+    ],
+)
+def test_run_package_new_command(
+    tmp_path: Path, output_dir: str, additional_test_cases: list[str]
+):
     output_dir = output_dir.format(tmp_path)
 
-    call_package_new(tmp_path, output_dir=output_dir)
+    call_package_new(
+        tmp_path, output_dir=output_dir, additional_test_cases=additional_test_cases
+    )
 
     pkg_dir = tmp_path / "test-package"
     if output_dir == "":
@@ -50,12 +66,16 @@ def test_run_package_new_command(tmp_path: Path, output_dir: str):
         Path("docs", "modules", "ROOT", "pages", "index.adoc"),
         Path("renovate.json"),
         Path("tests", "defaults.yml"),
-    ]
+    ] + [Path("tests", f"{case}.yml") for case in additional_test_cases]
 
     assert pkg_dir.is_dir()
     for f in expected_files:
-
         assert (pkg_dir / f).is_file()
+
+    with open(pkg_dir / ".github" / "workflows" / "test.yaml") as gh_test:
+        workflows = yaml.safe_load(gh_test)
+        instances = workflows["jobs"]["test"]["strategy"]["matrix"]["instance"]
+        assert instances == list(["defaults"] + additional_test_cases)
 
 
 @pytest.mark.parametrize(
@@ -86,8 +106,16 @@ def test_package_new_invalid_slug(config: Config, slug: str, expected: str):
 
 
 @pytest.mark.parametrize("golden", ["--golden-tests", "--no-golden-tests"])
-def test_lint_package_template(tmp_path: Path, golden: str):
-    call_package_new(tmp_path, golden=golden, output_dir=f"--output-dir={tmp_path}")
+@pytest.mark.parametrize("additional_test_cases", [[], ["foo"]])
+def test_lint_package_template(
+    tmp_path: Path, golden: str, additional_test_cases: list[str]
+):
+    call_package_new(
+        tmp_path,
+        golden=golden,
+        output_dir=f"--output-dir={tmp_path}",
+        additional_test_cases=additional_test_cases,
+    )
     pkg_dir = tmp_path / "test-package"
     exit_status = call("make lint", shell=True, cwd=pkg_dir)
     assert exit_status == 0
