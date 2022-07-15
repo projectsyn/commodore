5b2279b6821a1a1568e6dfde9566bc953662e21c
Simon Gerber <simon.gerber@vshn.ch>
2022-07-15 10:13:06 +0200

    Add tests for `package update`

--- tests/test_package_template.py
+++ tests/test_package_template.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 from pathlib import Path
 from subprocess import call
+from datetime import date
+
+import json
 
 import click
 import pytest
 import yaml
 
 from commodore.config import Config
+from commodore.gitrepo import GitRepo
 from commodore.package.template import PackageTemplater
 
 from conftest import RunnerFunc
@@ -148,3 +152,124 @@ def test_lint_package_template(
     pkg_dir = tmp_path / "test-package"
     exit_status = call("make lint", shell=True, cwd=pkg_dir)
     assert exit_status == 0
+
+
+@pytest.mark.parametrize(
+    "initial_golden,update_golden,expected_golden",
+    [
+        ("--golden-tests", "--no-golden-tests", False),
+        ("--no-golden-tests", "--golden-tests", True),
+    ],
+)
+def test_package_update_golden(
+    tmp_path: Path,
+    cli_runner: RunnerFunc,
+    initial_golden: str,
+    update_golden: str,
+    expected_golden: bool,
+):
+    call_package_new(
+        tmp_path,
+        cli_runner,
+        golden=initial_golden,
+        output_dir=f"--output-dir={tmp_path}",
+    )
+    pkg_dir = tmp_path / "test-package"
+
+    cli_runner(["-d", tmp_path, "package", "update", update_golden, str(pkg_dir)])
+
+    with open(pkg_dir / "renovate.json", "r", encoding="utf-8") as rjson:
+        renovatejson = json.load(rjson)
+        assert ("postUpgradeTasks" in renovatejson) == expected_golden
+        assert ("suppressNotifications" in renovatejson) == expected_golden
+
+
+def _verify_copyright_holder(pkg_dir: Path, holder="VSHN AG <info@vshn.ch>"):
+    year = date.today().year
+    with open(pkg_dir / "LICENSE", "r", encoding="utf-8") as lf:
+        line = lf.readline()
+        assert line == f"Copyright {year}, {holder}\n"
+
+
+def test_package_update_copyright_holder(
+    tmp_path: Path,
+    cli_runner: RunnerFunc,
+):
+    call_package_new(
+        tmp_path,
+        cli_runner,
+        output_dir=f"--output-dir={tmp_path}",
+    )
+    pkg_dir = tmp_path / "test-package"
+    _verify_copyright_holder(pkg_dir)
+
+    new_copyright = "Test Corp. <test@example.com>"
+    cli_runner(
+        [
+            "-d",
+            tmp_path,
+            "package",
+            "update",
+            "--copyright",
+            new_copyright,
+            str(pkg_dir),
+        ]
+    )
+
+    _verify_copyright_holder(pkg_dir, new_copyright)
+
+
+@pytest.mark.parametrize(
+    "initial_test_cases,additional_test_cases,remove_test_cases",
+    (
+        ([], ["foo"], []),
+        (["foo"], ["bar"], ["foo"]),
+        (["foo", "bar"], ["baz"], ["foo", "bar"]),
+    ),
+)
+def test_package_update_test_cases(
+    tmp_path: Path,
+    cli_runner: RunnerFunc,
+    initial_test_cases: list[str],
+    additional_test_cases: list[str],
+    remove_test_cases: list[str],
+):
+    # Create initial package
+    call_package_new(
+        tmp_path,
+        cli_runner,
+        golden="--golden-tests",
+        additional_test_cases=initial_test_cases,
+        output_dir=f"--output-dir={tmp_path}",
+    )
+    pkg_dir = tmp_path / "test-package"
+    atc_args = []
+    for case in additional_test_cases:
+        atc_args.extend(["-t", case])
+    dtc_args = []
+    for case in remove_test_cases:
+        dtc_args.extend(["--remove-test-case", case])
+
+    cli_runner(
+        ["-d", tmp_path, "package", "update", str(pkg_dir)] + atc_args + dtc_args
+    )
+
+    for case in set(initial_test_cases + additional_test_cases) - set(
+        remove_test_cases
+    ):
+        assert (pkg_dir / "tests" / f"{case}.yml").is_file()
+
+    for case in remove_test_cases:
+        assert not (pkg_dir / "tests" / f"{case}.yml").exists()
+
+    r = GitRepo(None, pkg_dir)
+    assert not r.repo.is_dirty()
+    assert len(r.repo.untracked_files) == 0
+    assert r.repo.head.commit.message.startswith("Update from template\n\n")
+
+
+def test_package_templater_from_existing_nonexistent(tmp_path: Path, config: Config):
+    with pytest.raises(click.ClickException) as e:
+        _ = PackageTemplater.from_existing(config, tmp_path / "test-package")
+
+    assert str(e.value) == "Provided package path isn't a directory"
