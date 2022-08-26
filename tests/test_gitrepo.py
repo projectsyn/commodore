d949ab43000a12dcb5ad31011eca05c038dcc11a
Simon Gerber <simon.gerber@vshn.ch>
2022-08-26 14:38:27 +0200

    Add test cases for `stage_all()` and `stage_files()` with merge conflict

--- tests/test_gitrepo.py
+++ tests/test_gitrepo.py
@@ -3,6 +3,7 @@ Unit-tests for git
 """
 from __future__ import annotations
 
+import subprocess
 from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import Optional
@@ -559,3 +560,48 @@ def test_gitrepo_commit_amend(tmp_path: Path):
     assert r.repo.head.commit.author.email == "john.doe@example.com"
     assert r.repo.head.commit.committer.name == "John Doe"
     assert r.repo.head.commit.committer.email == "john.doe@example.com"
+
+
+def _setup_merge_conflict(tmp_path: Path):
+    r, _ = setup_repo(tmp_path)
+    test_txt_path = r.working_tree_dir / "test.txt"
+    with open(test_txt_path, "w", encoding="utf-8") as f:
+        f.write("Hello, world!\n")
+    r.stage_all()
+    r.commit("Add content to test.txt")
+    assert not r.repo.is_dirty()
+
+    diff = """diff --git a/test.txt b/test.txt
+new file mode 100644
+index 0000000..d56c457
+--- /dev/null
++++ b/test.txt
+@@ -0,0 +1 @@
++Yo World!
+"""
+
+    result = subprocess.run(
+        ["git", "apply", "-3"], input=diff.encode(), cwd=r.working_tree_dir
+    )
+    # patch should not apply
+    assert result.returncode == 1
+
+    return r
+
+
+def test_gitrepo_stage_all_raises_on_conflict(tmp_path: Path):
+    r = _setup_merge_conflict(tmp_path)
+
+    with pytest.raises(gitrepo.MergeConflict) as e:
+        r.stage_all()
+
+    assert str(e.value) == "test.txt"
+
+
+def test_gitrepo_stage_files_raises_on_conflict(tmp_path: Path):
+    r = _setup_merge_conflict(tmp_path)
+
+    with pytest.raises(gitrepo.MergeConflict) as e:
+        r.stage_files(["test.txt"])
+
+    assert str(e.value) == "test.txt"
