af30ca7477d2c3276c1339fdf69dd25c72bbd6b0
Simon Gerber <simon.gerber@vshn.ch>
2025-07-11 10:00:39 +0200

    Add documentation for `commodore tool` command group

--- commodore/cli/tool.py
+++ commodore/cli/tool.py
@@ -16,6 +16,11 @@ from commodore.config import Config
 @options.pass_config
 @options.github_token
 def tool_group(config: Config, verbose: int, github_token: str):
+    """Commands to manage required external tools.
+
+    Currently, `helm`, jsonnet-bundler/`jb`, and `kustomize` are required
+    external tools.
+    """
     config.update_verbosity(verbose)
     config.managed_tools = tools.load_state()
     config.github_token = github_token
@@ -32,6 +37,16 @@ def tool_group(config: Config, verbose: int, github_token: str):
     help="Query GitHub API to get latest version",
 )
 def tool_list(config: Config, verbose: int, github_token: str, version_check: bool):
+    """List the version, location and management state of the required external tools.
+
+    Currently, `helm`, jsonnet-bundler/`jb`, and `kustomize` are required
+    external tools. By default, the command also queries the GitHub API to
+    determine the latest available version of each tool and indicates whether an
+    update is available.
+
+    Optionally, the command accepts a GitHub personal access token (PAT) to
+    avoid running into the fairly strict unauthenticated GitHub rate limits.
+    """
     config.update_verbosity(verbose)
     config.github_token = github_token
     tools.list_tools(config, version_check)
@@ -45,12 +60,25 @@ def tool_list(config: Config, verbose: int, github_token: str, version_check: bo
     "--version",
     default=None,
     metavar="VERSION",
-    help="A custom version to install for the requested tool (by default the latest version is installed)",
+    help="A version to install for the requested tool. "
+    + "By default, the latest version is installed.",
 )
 @click.argument("tool")
 def tool_install(
     config: Config, verbose: int, tool: str, version: Optional[str], github_token: str
 ):
+    """Install one of the required tools in `$XDG_CACHE_DIR/commodore/tools`.
+
+    The command will fail for tools which are already managed by Commodore.
+
+    By default, the command will install the latest available tool version. For
+    `helm` and `kustomize`, the command downloads the official installation
+    scripts and executes them with appropriate arguments. For `jb`, the command
+    directly downloads the requested version from the GitHub release page.
+
+    Optionally, the command accepts a tool version to install. The command
+    accepts versions prefixed  with "v" and unprefixed versions.
+    """
     config.update_verbosity(verbose)
     config.github_token = github_token
     tools.install_tool(config, tool, version)
@@ -64,12 +92,26 @@ def tool_install(
     "--version",
     default=None,
     metavar="VERSION",
-    help="A custom version to install for the requested tool (by default the latest version is installed)",
+    help="A version to upgrade (or downgrade) to for the requested tool. "
+    + "By default, the tool is upgraded to the latest version.",
 )
 @click.argument("tool")
 def tool_upgrade(
     config: Config, verbose: int, tool: str, version: Optional[str], github_token: str
 ):
+    """Upgrade (or downgrade) one of the required tools in `$XDG_CACHE_DIR/commodore/tools`.
+
+    The command will fail for tools which aren't managed by Commodore yet.
+
+    By default, the command will upgrade the tool to the latest available
+    version. For `helm` and `kustomize`, the command downloads the official
+    installation scripts and executes them with appropriate arguments. For `jb`,
+    the command directly downloads the requested version from the GitHub release
+    page.
+
+    Optionally, the command accepts a tool version to upgrade (or downgrade) to.
+    The command accepts versions prefixed with "v" and unprefixed versions.
+    """
     config.update_verbosity(verbose)
     config.github_token = github_token
     tools.upgrade_tool(config, tool, version)
